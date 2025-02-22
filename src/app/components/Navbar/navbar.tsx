7831a87457d54eb6ae9f9c4d25d918e67c397ade
Amandeep Singh <amandeep.s@zolostays.com>
2025-02-22 12:21:14 +0530

    Add Navbar component and update home page layout

---
+++ src/app/components/Navbar/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center p-4'>
+        <Link href="/">
+            <div className='text-2xl font-bold'>Amandeep</div>
+        </Link>
+        <div className='flex gap-4'>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/projects">Projects</Link>
+            <Link href="/contact">Contact</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Navbar from "./components/Navbar/navbar";
+
 export default function Home() {
   return (
-    <div>Amandeep Ka Portfolio</div>
+    <div>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Amandeep Ka Portfolio</h1>
+      </div>
+    </div>
   );
 }
